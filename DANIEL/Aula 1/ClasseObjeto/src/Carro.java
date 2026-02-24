65203ff1d4f34675bc77e1415aee91a64b03dc2d
Matheus Lombardi <matheus.profissional2022@gmail.com>
2026-02-24 20:33:51 -0300

    Aula 2 Daniel

--- DANIEL/Aula 1/ClasseObjeto/src/Carro.java
+++ DANIEL/Aula 1/ClasseObjeto/src/Carro.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Carro{
     //Variaveis
     public String marca, modelo;
@@ -12,8 +14,32 @@ public class Carro{
     }
     // Metodo da classe
     // Converte objeto em string
-    public String toString(){
+    public String toString() {
         return "Marca - " + this.marca + " Modelo - " + this.modelo +
-                " Velocidade - " + this.velocidade + " Motor - " + this.motor ;
+                " Velocidade - " + this.velocidade + " Motor - " + (this.motor ? "ligado" : "desligado");
+    }
+
+    public void toggleMotor(){
+        this.motor = !this.motor;
+
+        this.velocidade = !this.motor ? 0 : this.velocidade;
+        System.out.println(this.motor ? "Carro foi ligado" : "Carro foi Desligado");
+        System.out.println("Velocidade atual: " + this.velocidade);
+    }
+
+    public void acelerar(float acm){
+        if(this.motor){
+            this.velocidade = this.velocidade + acm;
+            System.out.println("Acelerando: " + acm  + "\nVelocidade atual: " + this.velocidade);
+        }
+        else System.out.println("Não foi possivel acelerar");
+    }
+    public void frear(float acm){
+        if (this.motor && acm <= this.velocidade) {
+            this.velocidade = this.velocidade - acm;
+
+            System.out.println("Freando: " + acm  + "\nVelocidade atual: " + this.velocidade);
+        }
+        else System.out.println("Não foi possivel frear");
     }
 }
--- DANIEL/Aula 1/ClasseObjeto/src/TestaCarro.java
+++ DANIEL/Aula 1/ClasseObjeto/src/TestaCarro.java
@@ -2,6 +2,7 @@ public class TestaCarro {
     public static void main(String args[]){
         //CRIA UMA INSTANCIA DA CLASSE CARRO
         Carro obj1 = new Carro("Fiat", "Uno");
+        obj1.toggleMotor();
         //obj1.marca = "Fiat";
         //obj1.modelo = "Uno";
         //obj1.velocidade = 0;
@@ -9,15 +10,26 @@ public class TestaCarro {
 
         System.out.println(obj1.toString());
 
+        System.out.println("////////////////////////////////////");
         Carro obj2 = new Carro("GM", "onix");
+        obj2.toggleMotor();
+        obj2.acelerar(20);
+        obj2.acelerar(20);
+        obj2.frear(20);
+        obj2.frear(20);
+
+        obj2.toggleMotor();
         //obj2.marca = "GM";
         //obj2.modelo = "Onix";
         //obj2.velocidade = 0;
         //obj2.motor = false;
 
         System.out.println(obj2.toString());
+        System.out.println("////////////////////////////////////");
 
         Carro obj3 = new Carro("Tesla", "CyberTruck");
+        obj3.toggleMotor();
+        obj3.toggleMotor();
         //obj3.marca = "Tesla"
         //obj3.modelo = "CyberTruck";
         //obj3.velocidade = 550;
@@ -25,4 +37,9 @@ public class TestaCarro {
 
         System.out.println(obj3.toString());
     }
+
 }
+
+//PLAY -> ACIONA O JDK COMPILA + INTERPRETA
+
+
