c4730fbdf467cad43bfcfe24681ebe93c7f02685
MATHEUS LOMBARDI DE SOUZA <26126@unifacef.edu.br>
2026-02-23 22:13:02 -0300

    alterações na aula 1 do daniel

--- DANIEL/Aula 1/ClasseObjeto/src/Carro.java
+++ DANIEL/Aula 1/ClasseObjeto/src/Carro.java
@@ -3,6 +3,13 @@ public class Carro{
     public String marca, modelo;
     public float velocidade;
     public boolean motor;
+
+    public Carro(String marca, String modelo){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidade = 0;
+        this.motor = false;
+    }
     // Metodo da classe
     // Converte objeto em string
     public String toString(){
