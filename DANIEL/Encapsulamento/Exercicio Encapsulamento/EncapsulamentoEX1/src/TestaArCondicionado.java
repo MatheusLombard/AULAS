f0940fc3dfd75c7adb23ebb543195330f00493a9
Matheus <matheus.profissional2022@gmail.com>
2026-03-08 22:13:44 -0300

    Ajustando ex1

--- DANIEL/Encapsulamento/Exercicio Encapsulamento/EncapsulamentoEX1/src/TestaArCondicionado.java
+++ DANIEL/Encapsulamento/Exercicio Encapsulamento/EncapsulamentoEX1/src/TestaArCondicionado.java
@@ -1,19 +1,14 @@
 public class TestaArCondicionado {
     public static void main(String[] args) {
 
-        // Criando um objeto do ar condicionado
         ArCondicionado ar = new ArCondicionado("AOC", "Dual Inverter", 22);
 
-        // Testando alteração de temperatura
         ar.setTemperatura(25);
 
-        // Testando temperatura fora da faixa
         ar.setTemperatura(35);
 
-        // Testando marca inválida
         ar.setMarca("A");
 
-        // Testando modo turbo
         ar.ativarModoTurbo();
 
 
