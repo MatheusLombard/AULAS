61cfc5360325e22d6efe448f5608a2140035c31c
MATHEUS LOMBARDI DE SOUZA <26126@unifacef.edu.br>
2026-03-18 20:38:05 -0300

    finalizando adicionar em linkedList

--- ALE/lib/LinkedList.mjs
+++ ALE/lib/LinkedList.mjs
@@ -28,12 +28,61 @@ export default class LinkedList{
     insert(posicao, valor){
         //cria o nodo para armazenar o valor pretendido
         const inserted = new Node(valor)
-
         //1° caso: a lista esta vazia
         if(this.isEmpty){
+            console.log('vazia')
             this.#head = inserted;
             this.#tail = inserted;
+        }else if(posicao === 0){
+            inserted.next = this.#head
+            this.#head = inserted
+
+        }else if(posicao >= this.#count){
+            this.#tail.next = inserted
+            this.#tail = inserted
+            console.log('maior que count')
+        }else{
+            let before = this.#head
+            console.log('no meio')
+            for(let i = 1; i < posicao; i++){
+                console.log('passou')
+
+                before = before.next
+            }
+            let after = before.next
+
+            inserted.next = after
+
+            before.next = inserted
+            
         }
-        this.count++;
+
+        this.#count++;
+    }
+    insertHead(valor){
+        this.insert(0, valor)
+    }
+    insertTail(valor){
+        this.insert(this.#count, valor)
+    }
+    remove(pos){
+        //1°Caso caso vazia ou não tem a posição
+        if(this.isEmpty || pos < 0 || pos > this.#count - 1){
+            return undefined
+        }
+        let removed 
+        //2°caso remover inicio da lista
+
+        if(pos === 0){
+            this.#head = this.#count.
+        }
     }
 }
+
+const teste = new LinkedList()
+teste.insert(0,6)
+teste.insert(1,5)
+teste.insert(2,2)
+teste.insert(3, 7)
+teste.insert(1, 7)
+teste.insert(2, 7)
